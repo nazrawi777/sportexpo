515c1385450ca344498125a61e7a4237d4c722b6
nazrawi777 <151239515+nazrawi777@users.noreply.github.com>
2026-07-02 13:08:32 +0000

    fixedblogdetail

--- config/settings.py
+++ config/settings.py
@@ -1,8 +1,9 @@
+from decouple import config
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-CHANGE-THIS-LATER'
+SECRET_KEY = config('SECRET_KEY')       
 
 DEBUG = True
 
